6287389b053d19eed3903cba322a637506824867
strothj <jason@jasons.io>
2018-03-22 01:17:27 -0400

    chore: add details to flow plugin detection hack

--- packages/rewire/rewirePreset.ts
+++ packages/rewire/rewirePreset.ts
@@ -15,7 +15,20 @@ const removeFlowPreset = (...args: any[]) => {
 
 export default removeFlowPreset;
 
-// @babel/plugin-transform-flow-strip-types
+/**
+ * Different versions of Babel 7 behave differently when it comes to supplying
+ * presets. Later versions wrap imported presets in a function that performs
+ * a Babel version check. As a result, we can not do a string comparison to
+ * determine if the supplied preset is the Flow plugin using the function name.
+ *
+ * Later versions of babel-preset-react-app have an option to disable the Flow
+ * plugin. To remain compatible with older react-scripts 2 beta versions, we do
+ * not use that feature. When react-scripts 2 is officially released, this hack
+ * should be removed.
+ *
+ * @see https://github.com/facebook/create-react-app/tree/next/packages/babel-preset-react-app#usage-with-typescript
+ * @see https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-flow-strip-types
+ */
 const babelPluginTransformFlowDetectionHack = (preset: any): boolean => {
   /* tslint:disable-next-line:no-var-requires */
   const babel: any = require("@babel/core");

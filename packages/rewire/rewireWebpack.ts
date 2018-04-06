4d55ee58b39e4c110637f27772c26137a9d3caf8
Alexander Roberts <detrohutt@gmail.com>
2018-04-06 01:12:36 -0500

    Add tsExtHighPriority option

--- packages/rewire/rewireWebpack.ts
+++ packages/rewire/rewireWebpack.ts
@@ -11,47 +11,67 @@ reactScriptsPaths.appIndexJs = reactScriptsPaths.appIndexJs.replace(
   `src${path.sep}index.tsx`
 );
 
-export default function(c: webpack.Configuration): webpack.Configuration {
-  // Validate and narrow type
-  const config = getValidatedConfig(c);
+export type CreateRewireWebpack = (
+  options: { tsExtHighPriority: boolean }
+) => RewireWebpack;
 
-  config.resolve.extensions.unshift(".web.ts", ".web.tsx", ".ts", ".tsx");
+export type RewireWebpack = (c: webpack.Configuration) => webpack.Configuration;
 
-  // Locate the Webpack loader responsible for handling Javascript assets and
-  // add TypeScript file extensions.
-  const scriptLoader = getLoader(config.module.rules, scriptsLoaderMatcher);
-  if (!scriptLoader) throw new Error("Unable to locate scripts loader.");
-  scriptLoader.test = /\.(ts|tsx|js|jsx|mjs)$/;
+export interface RewireWebpackWithOptions extends RewireWebpack {
+  withOptions?: CreateRewireWebpack;
+}
 
-  // Replace the babel-preset-react-app preset with the preset rewire from this
-  // package. This is done so @babel/preset-flow can be removed.
-  const babelLoader = getBabelLoader(config.module.rules) as webpack.NewLoader;
-  if (!babelLoader || !babelLoader.options)
-    throw new Error("Unable to locate Babel loader.");
-  babelLoader.options.presets = [path.resolve(__dirname, "rewirePreset")];
+const rewireWebpack = createRewireWebpack();
+(rewireWebpack as RewireWebpackWithOptions).withOptions = createRewireWebpack;
 
-  // Older versions of react-scripts v2 use a Webpack loader to add support for
-  // SVGs as React components. Later versions do this using a Babel plugin.
-  // Check if it is present. If it is, replace the preset in the SVG loader's
-  // sibling Babel loader.
-  const svgLoader = getLoader(config.module.rules, svgLoaderMatcher);
-  if (svgLoader) {
-    if (!("use" in svgLoader) || !Array.isArray(svgLoader.use))
-      throw new Error("Unexpected layout for SVG loader.");
-    const svgBabelLoader = svgLoader.use.find((l: any) =>
-      /babel-loader/.test(l.loader)
-    );
-    if (
-      !svgBabelLoader ||
-      typeof svgBabelLoader !== "object" ||
-      !("options" in svgBabelLoader) ||
-      svgBabelLoader.options == null
-    )
-      throw new Error("Unable to locate sibling Babel loader in SVG loader.");
-    svgBabelLoader.options.presets = babelLoader.options.presets;
-  }
+export default rewireWebpack;
 
-  return config;
+function createRewireWebpack(options = { tsExtHighPriority: false }) {
+  return (c => {
+    // Validate and narrow type
+    const config = getValidatedConfig(c);
+
+    const arrAddFn = options.tsExtHighPriority ? "unshift" : "push";
+    config.resolve.extensions[arrAddFn](".web.ts", ".web.tsx", ".ts", ".tsx");
+
+    // Locate the Webpack loader responsible for handling Javascript assets and
+    // add TypeScript file extensions.
+    const scriptLoader = getLoader(config.module.rules, scriptsLoaderMatcher);
+    if (!scriptLoader) throw new Error("Unable to locate scripts loader.");
+    scriptLoader.test = /\.(ts|tsx|js|jsx|mjs)$/;
+
+    // Replace the babel-preset-react-app preset with the preset rewire from this
+    // package. This is done so @babel/preset-flow can be removed.
+    const babelLoader = getBabelLoader(
+      config.module.rules
+    ) as webpack.NewLoader;
+    if (!babelLoader || !babelLoader.options)
+      throw new Error("Unable to locate Babel loader.");
+    babelLoader.options.presets = [path.resolve(__dirname, "rewirePreset")];
+
+    // Older versions of react-scripts v2 use a Webpack loader to add support for
+    // SVGs as React components. Later versions do this using a Babel plugin.
+    // Check if it is present. If it is, replace the preset in the SVG loader's
+    // sibling Babel loader.
+    const svgLoader = getLoader(config.module.rules, svgLoaderMatcher);
+    if (svgLoader) {
+      if (!("use" in svgLoader) || !Array.isArray(svgLoader.use))
+        throw new Error("Unexpected layout for SVG loader.");
+      const svgBabelLoader = svgLoader.use.find((l: any) =>
+        /babel-loader/.test(l.loader)
+      );
+      if (
+        !svgBabelLoader ||
+        typeof svgBabelLoader !== "object" ||
+        !("options" in svgBabelLoader) ||
+        svgBabelLoader.options == null
+      )
+        throw new Error("Unable to locate sibling Babel loader in SVG loader.");
+      svgBabelLoader.options.presets = babelLoader.options.presets;
+    }
+
+    return config;
+  }) as RewireWebpack;
 }
 
 // Matcher to find JavaScript/JSX loader using getLoader util from

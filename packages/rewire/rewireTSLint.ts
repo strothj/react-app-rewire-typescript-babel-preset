876cc3f823503fbd0ad3af650349d37b43002089
strothj jason@jasons.io <jason@jasons.io>
2018-09-26 04:50:07 -0400

    feat: add build fb6e6f70 support to tslint rewire

--- packages/rewire/rewireTSLint.ts
+++ packages/rewire/rewireTSLint.ts
@@ -12,6 +12,10 @@ export default function(
   // Grab the current ESLint config so we can copy some of its settings
   const esLintLoader = getLoader(config.module.rules, esLintLoaderMatcher);
 
+  if (!esLintLoader) {
+    throw new Error("Could not locate eslint-loader.");
+  }
+
   // Create a new rule
   const tsLintLoader: webpack.RuleSetRule = {
     test: /\.(ts|tsx)$/,
@@ -34,7 +38,11 @@ export default function(
 const esLintLoaderMatcher: Matcher = rule =>
   Boolean(
     rule.test &&
-      rule.test.toString() === /\.(js|jsx|mjs)$/.toString() &&
+      // TODO: Remove the rule regex test that includes the JavaScript modules
+      // extension. Versions prior to 2.0.0-next.fb6e6f70 included support for
+      // JavaScript modules.
+      (rule.test.toString() === /\.(js|jsx|mjs)$/.toString() ||
+        rule.test.toString() === /\.(js|jsx)$/.toString()) &&
       Array.isArray(rule.use) &&
       rule.use.find((r: any) => r.loader && /eslint-loader/.test(r.loader))
   );

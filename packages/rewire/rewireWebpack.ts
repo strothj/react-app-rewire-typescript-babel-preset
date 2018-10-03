9e9c07de369202df6dbb5b9d127bddc577517041
corjen <corjen@me.com>
2018-10-03 12:24:35 +0200

    Improve babel & script loader matcher

--- packages/rewire/rewireWebpack.ts
+++ packages/rewire/rewireWebpack.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import * as webpack from "webpack";
 import reactScriptsPaths from "react-scripts/config/paths";
-import { getLoader, Matcher } from "react-app-rewired";
+import { getLoader, Matcher, getBabelLoader } from "react-app-rewired";
 import { getValidatedConfig } from "./webpackUtils";
 
 // Switch out the entry point index.js for index.tsx.
@@ -20,23 +20,17 @@ export default function(c: webpack.Configuration): webpack.Configuration {
 
   // Locate the Webpack loader responsible for handling Javascript assets and
   // add TypeScript file extensions.
-
-  const rules = config.module.rules[3].oneOf;
-  if (!rules) {
-    throw new Error("Unable to locate rules");
-  }
-  const scriptLoader = getLoader(rules, scriptsLoaderMatcher);
+  const scriptLoader = getLoader(config.module.rules, scriptsLoaderMatcher);
   if (!scriptLoader) throw new Error("Unable to locate scripts loader.");
   scriptLoader.test = /\.(ts|tsx|js|jsx)$/;
 
   // Replace the babel-preset-react-app preset with the preset rewire from this
   // package. This is done so @babel/preset-flow can be removed.
-  const babelLoader = getLoader(rules, babelLoaderMatcher) as webpack.NewLoader;
-  if (!babelLoader || !babelLoader.options) {
+  const babelLoader = getBabelLoader(config.module.rules) as webpack.NewLoader;
+  if (!babelLoader || !babelLoader.options)
     throw new Error("Unable to locate Babel loader.");
-  }
-
   babelLoader.options.presets = [path.resolve(__dirname, "rewirePreset")];
+
   return config;
 }
 
@@ -45,13 +39,10 @@ export default function(c: webpack.Configuration): webpack.Configuration {
 // regular expression for its file name matching.
 const scriptsLoaderMatcher: Matcher = rule => {
   return Boolean(
-    rule && rule.test && rule.test.toString() === /\.(js|jsx)$/.toString()
+    rule.test &&
+      rule.test.toString() === /\.(js|jsx)$/.toString() &&
+      rule.enforce !== "pre" &&
+      typeof rule.loader === "string" &&
+      /babel-loader/.test(rule.loader)
   );
 };
-
-const babelLoaderMatcher: Matcher = rule =>
-  Boolean(
-    typeof rule.loader === "string" &&
-      (/babel-loader/.test(rule.loader) ||
-        /babel-preset-react-app.loader/.test(rule.loader))
-  );
